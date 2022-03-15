823ead2038af01da20a8a0012bcfc229ffdc0c88
HeRo3S <hungid6a1@gmail.com>
2022-03-15 22:59:34 +0700

    Added testing animation for square weapon
    New testing animation for square weapon
    Split control schemas into keyboard&mouse and joystick

--- Assets/Script/TestWeapon.cs
+++ Assets/Script/TestWeapon.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 public class TestWeapon : MonoBehaviour
 {
     private HashSet<GameObject> hitList = new HashSet<GameObject>();
+    Animator anim;
+
+
+    public void Start()
+    {
+        //setup animation controller
+        PlayerInputSystem weaponInput = new PlayerInputSystem();
+        weaponInput.Player.LightAttack.Enable();
+        weaponInput.Player.LightAttack.performed += LightAttack_performed;
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void LightAttack_performed(InputAction.CallbackContext obj)
+    {
+        anim.SetTrigger("LightAttack");
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject hitEntity = collision.gameObject;
@@ -29,4 +47,5 @@ public class TestWeapon : MonoBehaviour
             }
         }
     }
+
 }
