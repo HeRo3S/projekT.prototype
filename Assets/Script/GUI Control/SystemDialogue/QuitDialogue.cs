31ea257f1e016aa4d5095de0673d579008b7954b
HeRo3S <hungid6a1@gmail.com>
2022-05-21 00:52:42 +0700

    Quit dialogue has a return to title screen button.

--- Assets/Script/GUI Control/SystemDialogue/QuitDialogue.cs
+++ Assets/Script/GUI Control/SystemDialogue/QuitDialogue.cs
@@ -7,19 +7,26 @@ public class QuitDialogue : MonoBehaviour
 {
     Button backBtn;
     Button ragequitBtn;
+    Button mainmenuBtn;
     private void Awake()
     {
         backBtn = transform.Find("Panel").Find("BackButton").GetComponent<Button>();
         backBtn.onClick.AddListener(BackButtonOnClick);
         ragequitBtn = transform.Find("Panel").Find("RagequitButton").GetComponent<Button>();
         ragequitBtn.onClick.AddListener(RagequitButtonOnClick);
-
+        mainmenuBtn= transform.Find("Panel").Find("MenuButton").GetComponent<Button>();
+        mainmenuBtn.onClick.AddListener(BackToMainMenuButtonOnClick);
     }
 
     private void BackButtonOnClick()
     {
         CanvasController.GetInstance().EnableOnlyCanvas("IngameHUDCanvas");
         //GameStateManager.Instance.SwitchToStateIngame();
+    }
+
+    private void BackToMainMenuButtonOnClick()
+    {
+        ScenesController.Instance.LoadScene("TitleScene");
     }
     private void RagequitButtonOnClick()
     {
